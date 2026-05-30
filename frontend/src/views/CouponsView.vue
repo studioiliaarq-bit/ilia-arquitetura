2427002a4cfa73ec5895c3d2a8874f44b1da3158
studioiliaarq-bit <studioilia.arq@gmail.com>
2026-05-30 13:22:18 -0300

    Add files via upload

--- frontend/src/views/CouponsView.vue
+++ frontend/src/views/CouponsView.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="Coupons">  
-    
+  <div id="container-coupons">  
+     <div id="box-polygon">   
+      <PolygonAnimation />      
+    </div>
+    <Coupons />
+    <Footer />
   </div>
 </template> 
 
 
 <script>
+import PolygonAnimation from '@/components/PolygonAnimation.vue';
+import Coupons from '@/components/Coupons.vue';
+import Footer from '@/components/Footer.vue';
+
 export default {
   name: 'CouponsView',
 
   components: {
-    
+    PolygonAnimation, 
+    Coupons,
+    Footer,
   },
 
-  data() {
-    return {
-      westwingLink: 'https://www.westwing.com.br/',
-      westwingCoupon: 'MAGDANASCIMENTOWW'
-    }
-  }
 }
 </script>
 
 <style scoped>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#container-coupons {
+  background-image: url('../assets/concept-image.webp'); 
+  background-repeat: no-repeat;
+  background-size: cover;  
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  z-index: 99;
+}
+
+@media (max-width: 980px) {
+  #container-coupons {
+    background-image: url('../assets/apartamento-terra/cozinha/1.webp');
+    background-repeat: no-repeat;
+    background-size: cover;  
+  }
+}
+
+#box-polygon { 
+  width: 100%;
+  height: auto;  
+  margin: 80px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  z-index: 100;
+
+}
+
 
   
 </style>
